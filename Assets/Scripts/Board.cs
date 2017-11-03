77a394702017e714190365c02fd69c5c62fcb63b
azhry <arliansyah_azhary@yahoo.com>
2017-11-03 10:27:32 +0700

    Initialize prefabs and scripts

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+	public GameObject[] warZone;
+	public GameObject[] player0Zone;
+	public GameObject[] player1Zone;
+	private Controller controller;
+
+	public void SetControllerRef(Controller c) {
+		controller = c;
+	}
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public GameObject board;
+	private int playerSide;
+
+	void Awake() {
+		playerSide = Random.Range (0, 1);
+	}
+
+	public int GetPlayerSide() {
+		return playerSide;
+	}
+
+	void ChangeSides() {
+		playerSide ^= 1;
+	}
+
+	void SetControllerRef() {
+		board.GetComponent<Board> ().SetControllerRef (this);
+	}
+}
